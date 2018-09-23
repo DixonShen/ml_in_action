014e8bd9776b3abc2e6973bce0c1ba04bb5de58f
yongxin.shen <yongxin.shen@ele.me>
2018-09-23 12:53:44 +0800

    learn tf

--- Test/test03.py
+++ Test/test03.py
@@ -6,6 +6,12 @@
 # @File    : test03.py
 
 import time
-from tqdm import *
-for i in tqdm(range(1000)):
-       time.sleep(0.01)
+import re
+
+
+p1 = '([\u4E00-\u9FA5\uF900-\uFA2D\w]+)[\(|（|《]*'
+p2 = '\d*[-|弄]*\d+[栋|号楼|幢|号]+'
+s1 = '红山花园555弄21-29号'
+m1 = re.search(p2, s1)
+print(m1)
+print(m1.group(0))
